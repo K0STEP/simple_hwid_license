5e1e3ed473990c2552af5c124b08151013f19d72
k0step <liln0w140k@gmail.com>
2026-01-15 11:41:14 +0300

    created get_cur_hwid in main.py

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-"""
-HERE WILL BE THE MAIN BODY OF THE PROJECT
-"""
+import os
+import sys
+import hashlib
+import hmac
+import subprocess
+import uuid
+
+SECRET = b"my_pizdatiy_key"
+LICENSE_FILE = "license.key"
+
+def get_cur_hwid() -> str:
+    """
+    :return: MAC-adress
+    """
+    mac = hex(uuid.getnode())[2:].upper()
+    return mac
