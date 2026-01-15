157f2adb324086ee9b0945865e4d1f2fe639c4ad
k0step <liln0w140k@gmail.com>
2026-01-15 11:52:30 +0300

    created get_cur_hwid in main.py

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ import hmac
 import subprocess
 import uuid
 
-SECRET = b"my_pizdatiy_key"
+SECRET = b"MY_KEY"
 LICENSE_FILE = "license.key"
+
 
 def get_cur_hwid() -> str:
     """
@@ -14,3 +15,19 @@ def get_cur_hwid() -> str:
     """
     mac = hex(uuid.getnode())[2:].upper()
     return mac
+
+
+def validate_lic(lic_key: str, cur_hwid: str) -> bool:
+    """
+    check license key
+    :param lic_key: license key from LICENSE_FILE
+    :param cur_hwid: current hwid
+    :return: TRUE if lic_key is correct
+    """
+    hwid_from_key, signature = lic_key.rsplit(".", 1)
+    if hwid_from_key != cur_hwid:
+        return False
+    expected_sig = hmac.new(SECRET, hwid_from_key.encode(), hashlib.sha256).hexdigest()
+    return hmac.compare_digest(signature, expected_sig)
+
+
