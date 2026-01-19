b16b57951f269ef63dd84847fefba318e2eea65b
k0step <liln0w140k@gmail.com>
2026-01-19 13:25:08 +0300

    created example of usage in main

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 from hwid_license.core import HWIDL
+
+def main():
+    ...
+
+if __name__ == "__main__":
+    lic = HWIDL(secret=b'MY_KEY', license_file="license.key")
+    lic.check_valid()
+    main()
